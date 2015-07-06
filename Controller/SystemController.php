ceb3f7d5a5e712289955ff68c90bb17b9e0c2258
novuscom <info@novuscom.ru>
2015-07-06 18:17:15 +0000

    Добавлены команды для обновления

--- Controller/SystemController.php
+++ Controller/SystemController.php
@@ -25,8 +25,17 @@ class SystemController extends Controller
     public function upgradeAction()
     {
         $path = realpath($this->get('kernel')->getRootDir() . '/../');
-        $object = $this->runCommand($path, 'COMPOSER_HOME="' . $path . '" php composer.phar update --ansi');
-        $response = new Response(json_encode($object));
+        $commands = array(
+            'COMPOSER_HOME="' . $path . '" php composer.phar update --ansi',
+            'COMPOSER_HOME="' . $path . '" php composer.phar dump-autoload --optimize',
+            'php app/console cache:clear'
+        );
+        $result = array();
+        foreach ($commands as $com) {
+            $object = $this->runCommand($path, $com);
+            $result[] = $object;
+        }
+        $response = new Response(json_encode($result));
         $response->headers->set('Content-Type', 'application/json; charset=UTF-8');
         return $response;
     }
