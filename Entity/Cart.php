5bf03349ac4bec1021fee088bcd516dc56f3aef6
novuscom <info@novuscom.ru>
2015-08-02 16:11:47 +0000

    Создана сущность для корзины товаров

---
+++ Entity/Cart.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Novuscom\CMFBundle\Entity;
+
+/**
+ * Cart
+ */
+class Cart
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     */
+    private $updated;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Cart
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     *
+     * @return Cart
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+    /**
+     * @var \Novuscom\CMFUserBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Set user
+     *
+     * @param \Novuscom\CMFUserBundle\Entity\User $user
+     *
+     * @return Cart
+     */
+    public function setUser(\Novuscom\CMFUserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Novuscom\CMFUserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
