f5ab6ae812f16120c170197efb828fa3ab6a75a5
novuscom <info@novuscom.ru>
2015-08-10 17:07:09 +0000

    Получение текущей корзины пользователя через API #20

--- Services/Product.php
+++ Services/Product.php
@@ -8,6 +8,17 @@ use Novuscom\CMFBundle\Entity\ElementSection;
 class Product
 {
 
+    public function IfStoredInCart($element_id, $cart_id)
+    {
+        $element_reference = $this->em->getReference('Novuscom\CMFBundle\Entity\Element', $element_id);
+        $cart_reference = $this->em->getReference('Novuscom\CMFBundle\Entity\Cart', $cart_id);
+        $entity = $this->em->getRepository('NovuscomCMFBundle:Product')->findOneBy(array(
+            'element' => $element_reference,
+            'cart' => $cart_reference
+        ));
+        return $entity;
+    }
+
     public function GetByElementId($element_id)
     {
         if (is_numeric($element_id) == false)
