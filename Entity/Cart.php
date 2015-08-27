b5b770808c375d626926c16dee1a3f97e213a7a9
novuscom <info@novuscom.ru>
2015-08-27 14:37:03 +0000

    Поправлено форматирование

--- Entity/Cart.php
+++ Entity/Cart.php
@@ -7,181 +7,184 @@ namespace Novuscom\CMFBundle\Entity;
  */
 class Cart
 {
-    /**
-     * @var integer
-     */
-    private $id;
+	/**
+	 * @var integer
+	 */
+	private $id;
 
-    /**
-     * @var \DateTime
-     */
-    private $created;
+	/**
+	 * @var \DateTime
+	 */
+	private $created;
 
-    /**
-     * @var \DateTime
-     */
-    private $updated;
-
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return Cart
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-
-        return $this;
-    }
-
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Set updated
-     *
-     * @param \DateTime $updated
-     *
-     * @return Cart
-     */
-    public function setUpdated($updated)
-    {
-        $this->updated = $updated;
-
-        return $this;
-    }
-
-    /**
-     * Get updated
-     *
-     * @return \DateTime
-     */
-    public function getUpdated()
-    {
-        return $this->updated;
-    }
-    /**
-     * @var \Novuscom\CMFUserBundle\Entity\User
-     */
-    private $user;
+	/**
+	 * @var \DateTime
+	 */
+	private $updated;
 
 
-    /**
-     * Set user
-     *
-     * @param \Novuscom\CMFUserBundle\Entity\User $user
-     *
-     * @return Cart
-     */
-    public function setUser(\Novuscom\CMFUserBundle\Entity\User $user = null)
-    {
-        $this->user = $user;
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set created
+	 *
+	 * @param \DateTime $created
+	 *
+	 * @return Cart
+	 */
+	public function setCreated($created)
+	{
+		$this->created = $created;
 
-    /**
-     * Get user
-     *
-     * @return \Novuscom\CMFUserBundle\Entity\User
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    private $product;
+		return $this;
+	}
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->product = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+	/**
+	 * Get created
+	 *
+	 * @return \DateTime
+	 */
+	public function getCreated()
+	{
+		return $this->created;
+	}
 
-    /**
-     * Add product
-     *
-     * @param \Novuscom\CMFBundle\Entity\Product $product
-     *
-     * @return Cart
-     */
-    public function addProduct(\Novuscom\CMFBundle\Entity\Product $product)
-    {
-        $this->product[] = $product;
+	/**
+	 * Set updated
+	 *
+	 * @param \DateTime $updated
+	 *
+	 * @return Cart
+	 */
+	public function setUpdated($updated)
+	{
+		$this->updated = $updated;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Remove product
-     *
-     * @param \Novuscom\CMFBundle\Entity\Product $product
-     */
-    public function removeProduct(\Novuscom\CMFBundle\Entity\Product $product)
-    {
-        $this->product->removeElement($product);
-    }
+	/**
+	 * Get updated
+	 *
+	 * @return \DateTime
+	 */
+	public function getUpdated()
+	{
+		return $this->updated;
+	}
 
-    /**
-     * Get product
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getProduct()
-    {
-        return $this->product;
-    }
-    /**
-     * @var string
-     */
-    private $code;
+	/**
+	 * @var \Novuscom\CMFUserBundle\Entity\User
+	 */
+	private $user;
 
 
-    /**
-     * Set code
-     *
-     * @param string $code
-     *
-     * @return Cart
-     */
-    public function setCode($code)
-    {
-        $this->code = $code;
+	/**
+	 * Set user
+	 *
+	 * @param \Novuscom\CMFUserBundle\Entity\User $user
+	 *
+	 * @return Cart
+	 */
+	public function setUser(\Novuscom\CMFUserBundle\Entity\User $user = null)
+	{
+		$this->user = $user;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get code
-     *
-     * @return string
-     */
-    public function getCode()
-    {
-        return $this->code;
-    }
+	/**
+	 * Get user
+	 *
+	 * @return \Novuscom\CMFUserBundle\Entity\User
+	 */
+	public function getUser()
+	{
+		return $this->user;
+	}
+
+	/**
+	 * @var \Doctrine\Common\Collections\Collection
+	 */
+	private $product;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->product = new \Doctrine\Common\Collections\ArrayCollection();
+	}
+
+	/**
+	 * Add product
+	 *
+	 * @param \Novuscom\CMFBundle\Entity\Product $product
+	 *
+	 * @return Cart
+	 */
+	public function addProduct(\Novuscom\CMFBundle\Entity\Product $product)
+	{
+		$this->product[] = $product;
+
+		return $this;
+	}
+
+	/**
+	 * Remove product
+	 *
+	 * @param \Novuscom\CMFBundle\Entity\Product $product
+	 */
+	public function removeProduct(\Novuscom\CMFBundle\Entity\Product $product)
+	{
+		$this->product->removeElement($product);
+	}
+
+	/**
+	 * Get product
+	 *
+	 * @return \Doctrine\Common\Collections\Collection
+	 */
+	public function getProduct()
+	{
+		return $this->product;
+	}
+
+	/**
+	 * @var string
+	 */
+	private $code;
+
+
+	/**
+	 * Set code
+	 *
+	 * @param string $code
+	 *
+	 * @return Cart
+	 */
+	public function setCode($code)
+	{
+		$this->code = $code;
+
+		return $this;
+	}
+
+	/**
+	 * Get code
+	 *
+	 * @return string
+	 */
+	public function getCode()
+	{
+		return $this->code;
+	}
 }
