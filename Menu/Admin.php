16c19813977f49a09eadce617a030db10ec3a51f
novuscom <info@novuscom.ru>
2015-08-27 11:05:01 +0000

    Добавлен пункт меню для заказов

--- Menu/Admin.php
+++ Menu/Admin.php
@@ -10,111 +10,115 @@ use Novuscom\CMFBundle\Event\ConfigureMenuEvent;
 class Admin extends ContainerAware
 {
 
-    public function MainMenu(FactoryInterface $factory, array $options)
-    {
-        $em = $this->container->get('doctrine.orm.entity_manager');
+	public function MainMenu(FactoryInterface $factory, array $options)
+	{
+		$em = $this->container->get('doctrine.orm.entity_manager');
 
 
-        $request = $this->container->get('request');
-        $routeName = $request->get('_route');
-        $routeParams = $request->get('_route_params');
+		$request = $this->container->get('request');
+		$routeName = $request->get('_route');
+		$routeParams = $request->get('_route_params');
 
-        //echo '<pre>' . print_r($routeName, true) . '</pre>';
-        //echo '<pre>' . print_r($routeParams, true) . '</pre>';
+		//echo '<pre>' . print_r($routeName, true) . '</pre>';
+		//echo '<pre>' . print_r($routeParams, true) . '</pre>';
 
-        $siteIdExist = array_key_exists('site_id', $routeParams);
-
-
-        $User = $this->container->get('User');
-        $sites = $User->getUserSites();
+		$siteIdExist = array_key_exists('site_id', $routeParams);
 
 
-        /*
-         * Создаем меню
-         */
-        $menu = $factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'sidebar-menu');
+		$User = $this->container->get('User');
+		$sites = $User->getUserSites();
 
-        $menu->addChild('Пользователи', array(
-            'route' => 'admin_user'
-        ));
 
-        $pagesMenu = $menu->addChild('Страницы', array(
-            'route' => 'cmf_admin_site_list'
-        ))->setCurrent(true);
-        $pagesMenu->setAttribute('class', 'treeview'); // для li
-        if ($routeName == 'cmf_admin_site_pages') {
-            $pagesMenu->setAttribute('class', 'active');
-            //$pagesMenu->setCurrent(true);
-        }
-        $pagesMenu->setChildrenAttribute('class', 'treeview-menu');// для ul
+		/*
+		 * Создаем меню
+		 */
+		$menu = $factory->createItem('root');
+		$menu->setChildrenAttribute('class', 'sidebar-menu');
 
-        $blocksList = $menu->addChild('Инфоблоки', array(
-            'route' => 'admin_block'
-        ));
-        if ($this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
-            $menu->addChild('Группы пользователей', array(
-                'route' => 'admin_group'
-            ));
-            $menu->addChild('Сайты', array(
-                'route' => 'cmf_admin_site_list'
-            ));
+		$menu->addChild('Пользователи', array(
+			'route' => 'admin_user'
+		));
 
-            $menu->addChild('Маршруты', array(
-                'route' => 'admin_route'
-            ));
-        }
+		$pagesMenu = $menu->addChild('Страницы', array(
+			'route' => 'cmf_admin_site_list'
+		))->setCurrent(true);
+		$pagesMenu->setAttribute('class', 'treeview'); // для li
+		if ($routeName == 'cmf_admin_site_pages') {
+			$pagesMenu->setAttribute('class', 'active');
+			//$pagesMenu->setCurrent(true);
+		}
+		$pagesMenu->setChildrenAttribute('class', 'treeview-menu');// для ul
 
-        $blocks = $em->getRepository('NovuscomCMFBundle:Block')->findBySites($sites);
-        $blocksList->setAttribute('class', 'treeview'); // для li
-        if ($routeName == 'admin_block' || $routeName == 'admin_block_show') {
-            $blocksList->setAttribute('class', 'active');
-        }
-        $blocksList->setChildrenAttribute('class', 'treeview-menu');// для ul
-        $adminBlockShow = ($routeName == 'admin_block_show');
-        foreach ($blocks as $e) {
-            $blockItem = $blocksList->addChild($e->getName(), array(
-                'route' => 'admin_block_show',
-                'routeParameters' => array('id' => $e->getId())
-            ));
-            if ($adminBlockShow && $e->getId()==$routeParams['id']) {
-                $blockItem->setAttribute('class', 'active');
-            }
-        }
+		$blocksList = $menu->addChild('Инфоблоки', array(
+			'route' => 'admin_block'
+		));
+		if ($this->container->get('security.context')->isGranted('ROLE_ADMIN')) {
+			$menu->addChild('Группы пользователей', array(
+				'route' => 'admin_group'
+			));
+			$menu->addChild('Сайты', array(
+				'route' => 'cmf_admin_site_list'
+			));
 
-        $menuBlock = $menu->addChild('Меню', array(
-            'route' => 'cmf_admin_site_list'
-        ));
-        $menuBlock->setAttribute('class', 'treeview'); // для li
-        $menuBlock->setChildrenAttribute('class', 'treeview-menu');// для ul
+			$menu->addChild('Маршруты', array(
+				'route' => 'admin_route'
+			));
 
-        foreach ($sites as $e) {
-            $pageItem = $pagesMenu->addChild($e['name'], array(
-                'route' => 'cmf_admin_site_pages',
-                'routeParameters' => array('site_id' => $e['id'])
-            ));
-            if ($siteIdExist && $e['id'] == $routeParams['site_id']) {
-                $pageItem->setAttribute('class', 'active');
-            }
-            $menuBlockItem = $menuBlock->addChild($e['name'], array(
-                'route' => 'admin_menu',
-                'routeParameters' => array('site_id' => $e['id'])
-            ));
-        }
+			$menu->addChild('Заказы', array(
+				'route' => 'admin_order'
+			));
+		}
+
+		$blocks = $em->getRepository('NovuscomCMFBundle:Block')->findBySites($sites);
+		$blocksList->setAttribute('class', 'treeview'); // для li
+		if ($routeName == 'admin_block' || $routeName == 'admin_block_show') {
+			$blocksList->setAttribute('class', 'active');
+		}
+		$blocksList->setChildrenAttribute('class', 'treeview-menu');// для ul
+		$adminBlockShow = ($routeName == 'admin_block_show');
+		foreach ($blocks as $e) {
+			$blockItem = $blocksList->addChild($e->getName(), array(
+				'route' => 'admin_block_show',
+				'routeParameters' => array('id' => $e->getId())
+			));
+			if ($adminBlockShow && $e->getId() == $routeParams['id']) {
+				$blockItem->setAttribute('class', 'active');
+			}
+		}
+
+		$menuBlock = $menu->addChild('Меню', array(
+			'route' => 'cmf_admin_site_list'
+		));
+		$menuBlock->setAttribute('class', 'treeview'); // для li
+		$menuBlock->setChildrenAttribute('class', 'treeview-menu');// для ul
+
+		foreach ($sites as $e) {
+			$pageItem = $pagesMenu->addChild($e['name'], array(
+				'route' => 'cmf_admin_site_pages',
+				'routeParameters' => array('site_id' => $e['id'])
+			));
+			if ($siteIdExist && $e['id'] == $routeParams['site_id']) {
+				$pageItem->setAttribute('class', 'active');
+			}
+			$menuBlockItem = $menuBlock->addChild($e['name'], array(
+				'route' => 'admin_menu',
+				'routeParameters' => array('site_id' => $e['id'])
+			));
+		}
 
 		$menu->addChild('Система', array(
 			'route' => 'admin_system'
 		));
 
 
-        /*
-         * Создаем событие
-         */
-        $this->container->get('event_dispatcher')->dispatch(
-            ConfigureMenuEvent::CONFIGURE,
-            new ConfigureMenuEvent($factory, $menu)
-        );
-        return $menu;
-    }
+		/*
+		 * Создаем событие
+		 */
+		$this->container->get('event_dispatcher')->dispatch(
+			ConfigureMenuEvent::CONFIGURE,
+			new ConfigureMenuEvent($factory, $menu)
+		);
+		return $menu;
+	}
 
 }
