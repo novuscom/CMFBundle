c5ed3fc1b3cabe21b0719706f03f7e5caf7bcaf2
novuscom <info@novuscom.ru>
2015-08-28 07:51:20 +0000

    Добавлен сервис для вспомогательных функций

---
+++ Services/Utils.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Novuscom\CMFBundle\Services;
+
+use Monolog\Logger;
+
+class Utils
+{
+	CONST VERSION = '0.0.5.9';
+
+	public function getVersion(){
+		return self::VERSION;
+	}
+
+	private $logger;
+
+	public function __construct(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+}
