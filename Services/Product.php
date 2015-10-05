0455d29441577cd1e69e26fb2b4d20d027972b9d
novuscom <info@novuscom.ru>
2015-10-05 12:19:38 +0000

    Реализовано удаление товара из корзины (#35)

--- Services/Product.php
+++ Services/Product.php
@@ -4,9 +4,43 @@ namespace Novuscom\CMFBundle\Services;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Novuscom\CMFBundle\Entity\ElementSection;
+use Novuscom\CMFBundle\Services\Cart;
 
 class Product
 {
+
+	public function getProduct($productId, $cartId=false)
+	{
+		if ($cartId)
+			$cart_reference = $this->em->getReference('Novuscom\CMFBundle\Entity\Cart', $cartId);
+		else {
+			$cart_reference = $this->Cart->GetCurrent();
+		}
+		if (count($cart_reference)!=1)
+			return false;
+		$this->setCart($cart_reference);
+		$product = $this->em->getRepository('NovuscomCMFBundle:Product')->findOneBy(array(
+			'id' => $productId,
+			'cart' => $cart_reference
+		));
+		return $product;
+	}
+
+	private $cart;
+
+	public function setCart($cart){
+		$this->cart = $cart;
+	}
+
+	public function getCart(){
+		return $this->cart;
+	}
+
+	public function removeEntity($productEntity)
+	{
+		$this->em->remove($productEntity);
+		$this->em->flush();
+	}
 
 	public function IfStoredInCart($element_id, $cart_id = false)
 	{
@@ -41,9 +75,11 @@ class Product
 	}
 
 	private $em;
+	private $Cart;
 
-	public function __construct(\Doctrine\ORM\EntityManager $em)
+	public function __construct(\Doctrine\ORM\EntityManager $em, Cart $Cart)
 	{
 		$this->em = $em;
+		$this->Cart = $Cart;
 	}
 }
