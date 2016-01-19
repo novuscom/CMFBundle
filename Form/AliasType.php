dcf226edc9b48ba42b77a38057bf3ee2fb3127b5
novuscom <info@novuscom.ru>
2016-01-19 15:43:38 -0500

    Удалены маркеры конфликтов

--- Form/AliasType.php
+++ Form/AliasType.php
@@ -3,42 +3,6 @@
 namespace Novuscom\CMFBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-<<<<<<< HEAD
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-
-class AliasType extends AbstractType
-{
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-
-        $builder->add('name', 'text',  array('label'=>false));
-
-
-    }
-
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'Novuscom\CMFBundle\Entity\Alias'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'cmf_sitebundle_alias';
-    }
-=======
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -69,5 +33,4 @@ class AliasType extends AbstractType
 	{
 		return 'cmf_sitebundle_alias';
 	}
->>>>>>> 6b120d6339f9c8f270f714255a66ce26fbe4eb5c
 }
