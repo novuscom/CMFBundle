4edcba665649dda1913661b002ba8c825a009aeb
novuscom <info@novuscom.ru>
2016-05-30 13:27:21 -0400

    Изменена версия бандла

--- Services/Utils.php
+++ Services/Utils.php
@@ -6,7 +6,7 @@ use Monolog\Logger;
 
 class Utils
 {
-	CONST VERSION = '0.0.7.4';
+	CONST VERSION = '0.0.7.5';
 
 	public static function msg($object){
 		echo '<pre>'.print_r($object, true).'</pre>';
